9543495d3246a0d4f8f028ca329a3dbf0fe47485
chenchaocc <ccerdong@sina.cn>
2015-06-13 01:26:36 +0800

    Initial Commit

---
+++ Singleton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Singleton
+//
+//  Created by mac on 15/6/13.
+//  Copyright (c) 2015年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Singleton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Singleton
+//
+//  Created by mac on 15/6/13.
+//  Copyright (c) 2015年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
